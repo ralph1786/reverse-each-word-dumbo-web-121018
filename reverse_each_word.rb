cecb220b2a2faf1507cee7aea03aa28e4fccc06c
Rafael Cruz <ralph1786@gmail.com>
2018-11-20 23:55:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(string)
-  reversed_words = string.split(" ").collect { |word| word.reverse}.join("")
+  reversed_words = string.split(" ").collect { |word| word.reverse}.join(" ")
   return reversed_words
 end
